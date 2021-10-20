86477b6e659954c56bfb769d4ccd4df3859c546d
SauravNaruka <saurav.Naruka@craftywing.com>
2021-10-20 06:56:08 +0530

    Update styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,7 @@ const Home: NextPage = () => {
     <div className={styles.container}>
       <Head>
         <title>Crafty Wing</title>
+        <meta name="theme-color" content="#f4f9fa" />
         <meta
           name="description"
           content="Luxury Wood Furniture Online. Buy Hardwood furniture Online or from store near you in Jaipur. Get Sheesham furniture for the homes of your dream."
