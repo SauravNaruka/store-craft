bf1da54921608f4d98f4ec56778c57ebaf79a007
SauravNaruka <saurav.Naruka@craftywing.com>
2021-10-21 05:52:38 +0530

    Update test description

--- __tests__/components/IconButton.test.tsx
+++ __tests__/components/IconButton.test.tsx
@@ -15,7 +15,7 @@ describe('IconButton', () => {
     expect(screen.getByRole('button')).toHaveAccessibleName(randomText)
     expect(screen.getByRole('button')).toHaveClass(randomClassName)
   })
-  test('render of the button tag with accesable name and css class', () => {
+  test('render of the button tag with no css class', () => {
     const randomText = faker.random.words()
     render(<IconButton name={randomText}>Some Icon</IconButton>)
     expect(screen.getByRole('button')).toBeInTheDocument()
