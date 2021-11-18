5235597fc577fd0a86f57a067368f136d99335ac
CraftyWing <92083925+CraftyWing@users.noreply.github.com>
2021-11-18 17:56:13 +0530

    Update next version (#10)
    * Update next version
    
    * Add file .npmrc

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,32 +1,17 @@
 // eslint-disable-next-line @next/next/no-document-import-in-page
-import Document, {
-  DocumentContext,
-  Html,
-  Head,
-  Main,
-  NextScript,
-} from 'next/document'
+import {Html, Head, Main, NextScript} from 'next/document'
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
-
-    return {...initialProps}
-  }
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <meta name="theme-color" content="#f4f9fa" />
-          <link rel="icon" href="/favicon.png" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="theme-color" content="#f4f9fa" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
-
-export default MyDocument
