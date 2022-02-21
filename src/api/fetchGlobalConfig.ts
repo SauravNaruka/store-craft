0162c9ae32612f3c19ce081de760a42fea5a190b
SauravNaruka <saurav.Naruka@craftywing.com>
2022-02-21 06:06:50 +0530

    Remove debug logs

--- src/api/fetchGlobalConfig.ts
+++ src/api/fetchGlobalConfig.ts
@@ -7,7 +7,6 @@ const configID = process?.env?.SANITY_STUDIO_GLOBAL_CONFIG_ID ?? ''
 
 export async function fetchGlobalConfig(): Promise<GlobalConfig> {
   try {
-    console.log('-----------Starting fetchGlobalConfigQuery-----------')
     const navigation = await fetchGlobalConfigQuery()
     return navigation
   } catch (error) {
@@ -18,20 +17,11 @@ export async function fetchGlobalConfig(): Promise<GlobalConfig> {
 
 function fetchGlobalConfigQuery() {
   return client.GlobalConfig({id: configID}).then(globalConfigsQuery => {
-    console.log('----------- fetchGlobalConfigQuery sucess-----------')
-    console.log(`----------- configID = ${configID}-----------`)
-    const value = JSON.stringify(globalConfigsQuery)
-    console.log(` - values ${value}`)
-    Object.keys(globalConfigsQuery).forEach((key: string, index) => {
-      console.log(` -- keys ${index}: key=${key}`)
-    })
-    console.log('----------- fetchGlobalConfigQuery value ends-----------')
     const globalConfig = globalConfigsQuery.GlobalConfig
-    console.log(`-----------globalConfig: ${globalConfig}-----------`)
+
     if (globalConfig) {
       return globalConfig as GlobalConfig
     }
-    console.log(`-----------Not a globalConfig-----------`)
 
     throw new Error(
       `${API_RESPONSE_ERROR}. request at client.GlobalConfigs failed`,
